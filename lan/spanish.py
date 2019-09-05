ed395d806682a45d3b4e2eb33824b8afcded71e2
max-chromebook <analogtaco@gmail.com>
2019-09-05 07:55:34 -0400

    first commit

---
+++ lan/spanish.py
@@ -0,0 +1,19 @@
+# fuction words
+
+article = {
+    'el' : 'the',
+    'la' : 'the',
+    'un' : ['a', 'an']
+}
+
+conjunction = {
+    'que' : ['that', 'which'],
+    'y' : 'and',
+    'como' : ['like', 'as'],
+    'pero' : ['but', 'yet', 'except'],
+    'o' : 'or',
+    'si' : ['if', 'whether'],
+    'porque' : 'because',
+    'cuando' : 'when',
+    'ni' : ['not even', '']
+}
---
+++ main.py
@@ -0,0 +1,12 @@
+from os import listdir
+import random
+
+# lists all available language modules
+lanlist = listdir('lan')
+
+def mainMenu {
+
+}
+
+# pull random dict entry
+random = random.choice(list(lan.article.key()))
