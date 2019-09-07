f617228efaccba2e6ba447fa12b8647fb6ffa54d
Maksym <analogtaco@gmail.com>
2019-09-07 00:25:55 -0400

    laptop backup

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from os import listdir
 import random
 
-# lists all available language modules
 lanlist = listdir('lan')
 
-def mainMenu {
+def match():
+    score = 0
+    #while usrinput not '0':
+    random = random.choice(list(lan.article.key()))# pull random dict entry
+    usrinput = (input("type in definition")).lower
+        #if usrinput is lan.article[random]:
+        #    score += 1
+        #    print("Correct! Current Score is: " + score)
+        #else:
+           # score -= 1
+           # print("Correct answer was: " + lan.article[random])
+           # print("Current score is: " + score)
+# imports the specific language module
+def lanselect(lan):
+	import lan
+	print("Succesfully loaded the " + lan + " Language!")
+	mainMenu()
 
-}
+# lists available lan modules
+def availan():
+	print("Available Languages: ")
+	for i in range(len(lanlist)): # prints out a legible index value and available Languages
+		print(i, end = " ")
+		print(lanlist[i])
+	lanselect(lanlist[int(input("Enter index value of the desired language: "))].strip('.py')
 
-# pull random dict entry
-random = random.choice(list(lan.article.key()))
+# main menu function
+def mainMenu():
+	print("Main Menu")
+	availan()
+	match()
+
+mainMenu()
